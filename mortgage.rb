8a7aa5c9a9d342c83154b75e87d23cb1e83e0507
Emil Hellsten <eba.paasi@gmail.com>
2022-02-16 10:46:00 +0000

    Added formula to mortgage

---
+++ mortgage.rb
@@ -0,0 +1,5 @@
+# formula: monthly_payment = loan_amount * (monthly_interest / (1 - (1 + monthly_interest)**(-loan_duration_months)))
+
+# gets x3 (total loan amount , interest/month and duration in months)
+
+
