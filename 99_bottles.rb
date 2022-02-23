a960faaf2ce6d56f453050b0a8eaaac7df752f9f
Emil Hellsten <eba.paasi@gmail.com>
2022-02-23 11:20:28 +0000

    Pushed 99 bottles to git

---
+++ 99_bottles.rb
@@ -0,0 +1,15 @@
+    def bottles_of_beer 
+
+      i = 99
+
+      while i < 99 and i > 0 
+
+        puts "#{a} bottles of beer on the wall. #{a} bottle of beer."
+
+      i = i - 1
+
+        puts "Take one down, pass it around. #{i} bottle of beer on the wall."
+
+      end
+
+    end
