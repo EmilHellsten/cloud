7e0ed2f8bef82c134bbb7a44028104196ccaf3fb
EmilHellsten <98455448+EmilHellsten@users.noreply.github.com>
2022-02-25 16:34:11 +0200

    Create angry_boss.rb

---
+++ angry_boss.rb
@@ -0,0 +1,12 @@
+  GNU nano 2.9.8                                                                 angry_boss.rb                                                                            
+
+puts("Pls ask boss for something.")
+q = gets.chomp.upcase
+
+puts("WHADDYA MEAN #{q}!? YOU'RE FIRED!")
+
+
+
+
+
+
