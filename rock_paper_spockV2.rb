407a1b4e73a3cfb30477025409cad8a831c34434
EmilHellsten <98455448+EmilHellsten@users.noreply.github.com>
2022-03-04 15:35:36 +0200

    Update rock_paper_spockV2.rb

--- rock_paper_spockV2.rb
+++ rock_paper_spockV2.rb
@@ -1,8 +1,9 @@
 VALID_CHOICES = %w(rock lizard spock scissors paper)
 
-#TO_DO_LIST: Typing the full word "rock" or "lizard" is tiring. Update the program so the user can type "r" for "rock," "p" for "paper," etc. Note that if you do bonus #1, you'll have two words that start with "s." How do you resolve that?
-#MAP input r/s/p/l/sp -> rock scissors ...
-#switched to symbols as keys... if key = string need =>
+def prompt(message)
+  puts("==> #{message}")
+end
+
 win_conditions = {
   "rock" => ["lizard", "scissors"],
   "lizard" => ["spock", "paper"],
@@ -11,21 +12,37 @@ win_conditions = {
   "paper" => ["rock", "spock"]
 }
 
-def prompt(message)
-  puts("==> #{message}")
-end
-
 computer_score = 0
 human_score = 0
 choice = ''
-#TÄÄ EI OIKEESTAAN TOIMI keep_playing = true
 
-#outer loop
-while human_score < 3 && computer_score < 3 #does not exit loop at >3 OISKO joku keep_playing = true ja break human || comp == 3
+#contractions = {
+ # "rock" => ["r", "ro", "roc"],
+  #"lizard" => ["l", "li", "liz","liza","lizar"],
+  #"spock" => ["sp","spo", "spoc"],
+  #"scissors" => ["sc","sci", "scis", "sciss", "scisso", "scissor"],
+  #"paper" => ["p", "pa", "pap", "pape"]
+#}
+while human_score < 3 && computer_score < 3
   
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp.downcase
+   
+    case choice
+      when choice == "r" || "ro" || "roc"
+        choice = "rock"
+      when choice == "l" || "li" || "liz" || "liza" || "lizar"
+        choice = "lizard"
+      when choice == "sp" || "spo" || "spoc"
+        choice = "spock"
+      when choice == "sc" || "sci" || "scis" || "sciss" || "scisso" || "scissor"
+        choice = "scissors"
+      when choice == "p" || "pa" || "pap" || "pape"
+        choice = "paper"
+    end
+    
+    
     if VALID_CHOICES.include?(choice)
     break
     else prompt("That's not valid.")
@@ -33,7 +50,7 @@ while human_score < 3 && computer_score < 3 #does not exit loop at >3 OISKO joku
   end
 
   computer_choice = VALID_CHOICES.sample
-  
+
   prompt("You chose: #{choice}; computer chose: #{computer_choice}.")
 
   if choice == computer_choice
@@ -46,5 +63,7 @@ while human_score < 3 && computer_score < 3 #does not exit loop at >3 OISKO joku
     computer_score = computer_score + 1
   end 
   prompt("Human score is: #{human_score}; Computer score is #{computer_score}.")
-  # TÄÄ EI TOIMI JOS EKA LINE ON KEEP_PLAYING break if human_score || computer_score == 3
-end #LOPPUUN JOKU CONGRATS ON WINNING/LOSING/JOTAIN
+
+end
+
+prompt("Thanks for playing.")
